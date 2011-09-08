b7a30a6217718c8feaf3b033e8c42473f27f6e96
philipp.thomas.uni@googlemail.com <philipp.thomas.uni@googlemail.com@b2bae8db-34e6-dc36-b35c-51716d90a6ac>
2011-09-08 14:56:07 +0000

    - Struct hinzugefügt

---
+++ trunk/AntsTSP/Structs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AntsTSP
+{
+    public struct City
+    {
+        public Point koord;
+        public double phero;
+        public double distance;
+        public double atractivity;
+    }
+}
