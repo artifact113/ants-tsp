834c54547814ccc8f5aace76e49be0772660143c
philipp.thomas.uni@googlemail.com <philipp.thomas.uni@googlemail.com@b2bae8db-34e6-dc36-b35c-51716d90a6ac>
2011-10-13 16:29:30 +0000

    -grobes Konstrukt zum schreiben des Ergebnises in xml

---
+++ trunk/AntsTSP/OutputWriter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AntsTSP
+{
+    public static class OutputWriter
+    {
+        public static void Write(OutputData outputData, string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(OutputData));
+            FileStream stream = new FileStream(@path, FileMode.Create);
+            serializer.Serialize(stream, outputData);
+        }
+    }
+}
